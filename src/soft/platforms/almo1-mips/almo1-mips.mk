ed3f25f3704138f4aee49df4af880a9d51c29d83
franck <franck.wajsburt@lip6.fr>
2023-08-03 20:55:17 +0200

    clean Makefile...

--- src/soft/platforms/almo1-mips/almo1-mips.mk
+++ src/soft/platforms/almo1-mips/almo1-mips.mk
@@ -2,7 +2,7 @@ CC     = mipsel-unknown-elf-gcc#		compiler
 LD     = mipsel-unknown-elf-ld#			linker
 OD     = mipsel-unknown-elf-objdump#    desassembler
 RL     = mipsel-unknown-elf-ranlib#     index of library
-RL     = echo#     						if no ranlib required
+RL     = touch#     					if no ranlib required
 
 CFLAGS =    -mips32r2#				define of MIPS version
 CFLAGS +=   -G0#				    do not use global data pointer ($28)
