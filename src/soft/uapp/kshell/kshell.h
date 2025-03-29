27370b4244997a92d04e5f208705652d1c9275e2
gwynplaine <mnleonr@uni.pe>
2025-03-29 12:56:41 +0100

    constructors and destructors for statements. also some docs on the laters. not big deal.

--- src/soft/uapp/kshell/kshell.h
+++ src/soft/uapp/kshell/kshell.h
@@ -7,6 +7,12 @@
  * 0 -> ok yippee
  * other -> no ok no yippee
  * 
+ * those structures are shamelessly plumdered from 
+ * the Francois Normand's code that implemented a
+ * ast in order to parse the instructions. Even 
+ * if the control flow was not implemented, the 
+ * basic idea remains. So, if you feel stuck, take 
+ * a look to Normand's code.
  */
 
 
@@ -58,10 +64,11 @@ struct wordlist {
 };
 
 enum stmt_type {
-    WHILE_TYPE,          /* while block   */
-    IF_TYPE,             /* if block      */
-    BUILT_IN_TYPE,       /* built-in fux  */
-    EXEC_TYPE,           /* exec program  */
+    NULL_TYPE,           /*      sentinel */
+    WHILE_TYPE,          /*   while block */
+    IF_TYPE,             /*      if block */
+    BUILT_IN_TYPE,       /*  built-in fux */
+    EXEC_TYPE,           /*  exec program */
 };
 
 /* struct for a generic statement */
@@ -74,6 +81,7 @@ struct stmt {
         /* simple stmts */
         struct wordlist *simple_stmt;
     } stmt;
+    struct stmt *nxt;
 };
 
 /**
@@ -101,12 +109,20 @@ struct expr {
  * remember this is meant to be continued 
  * after you.
  */
+
+ /**
+  * \brief if statement structure
+  */
 struct if_stmt {
     struct expr *condition;    /* condition */
     struct stmt *true_case;    /* true branch */
     struct stmt *false_case;   /* false branch */
 };
 
+/**
+ * \brief while statement structure
+ * 
+ */
 struct while_stmt {
     struct expr *condition;     /* condition */
     struct stmt *execute;       /* execute */
@@ -123,13 +139,149 @@ typedef struct expr expr_s;
 
 
 /* wordlist outils */
+
+/**
+ * \brief   function to create a wordlist struct
+ * \return  A pointer to the newly wordlist. NULL on error.
+ * \note    if succeed nxt will be pointing to NULL.
+ */
 struct wordlist *wordlist_create(void);
+
+/**
+ * \brief   frees the wordlist until it finds nxt == NULL
+ * \param   wordlist the "head" of the list
+ * \note    this function will also free word. This function
+ *          will the destroy the wordlist from the wordlist
+ *          to the end of the list. If there's something 
+ *          behind, it will not be destroyed.
+ */
 void wordlist_destroy(struct wordlist *wordlist);
+
+/**
+ * \brief   helper function to print a wordlist until it finds
+ *          nxt == NULL
+ * \param   wordlist the head of the list
+ * \note    just for debug :P
+ */
 void wordlist_print(struct wordlist *wordlist);
 
 /* maker functions */
 
+/**
+ * \brief   creates a wordlist struct with word=str
+ * \param   str the head of the list
+ * \return  A pointer to the newly wordlist. NULL on error.
+ * \note    this function will alloc memory for word and then
+ *          copy the content of str. be sure to make a free to
+ *          str if you don't need it any longer.
+ */
 struct wordlist *make_wordlist(const char *str);
+
+/**
+ * \brief   creates a wordlist struct and puts it in the head 
+ *          of the list
+ * \param   l head of the list
+ * \param   str the string will be put into the word attribute 
+ *          of the newly wordlist
+ * \return  A pointer to the newly wordlist. NULL on error.
+ * \note    this function uses make_wordlist to create the new
+ *          wordlist. If this function fails, l will be unaltered
+ */
 struct wordlist *wordlist_pushfront(struct wordlist * l, char *str);
+
+/* statement related stuff */
+
+/**
+ * \brief   function to create a generic stmt struct
+ * \return  A pointer to the newly generic stmt. NULL on error.
+ * \note    if succeed nxt will be pointing to NULL and the 
+ *          type will be set to NULL_TYPE
+ */
+stmt_s *stmt_create(void);
+
+/**
+ * \brief   destroy a stmt
+ * \param   victim the stmt to destroy
+ * \note    this fuction destroys only the victim and 
+ *          will not touch the next stmt. On the contrary, 
+ *          it will erase all under the block if it's a flow control
+ *          statement. This function may call while_stmt_destroy and
+ *          if_stmt_destroy and itself.
+ *          If stmt is NULL, this function has no effect.
+ */
+void stmt_destroy(stmt_s *victim);
+
+/**
+ * \brief   function to create an if_stmt struct
+ * \return  A pointer to the newly generic if_stmt. NULL on error.
+ * \note    if_case and else_case will be pointing to NULL
+ */
+stmt_s *if_stmt_create(void);
+
+/**
+ * \brief   destroy an if_stmt
+ * \param   victim the if_stmt to destroy
+ * \note    this fuctions destroys the victim and all the 
+ *          conditional block it represents.
+ *          If stmt is NULL, this function has no effect.
+ *          this function may call stmt_destroy().
+ *          note that the nxt attribute will remain unmodified.
+ */
+void if_stmt_destroy(if_stmt_s *victim);
+
+/**
+ * \brief   function to create an while_stmt struct
+ * \return  A pointer to the newly generic while_stmt. NULL on error.
+ * \note    condition and execute will be pointing to NULL
+ */
+stmt_s *while_stmt_create(void);
+
+/**
+ * \brief   destroy an while_stmt
+ * \param   victim the while_stmt to destroy
+ * \note    this fuctions destroys the victim and all the 
+ *          conditional block it represents.
+ *          If stmt is NULL, this function has no effect.
+ *          this function may call stmt_destroy().
+ *          note that the nxt attribute will remain unmodified.
+ */
+void while_stmt_destroy(while_stmt_s *victim);
+
+/**
+ * \brief   set up a stmt to hold a simple statement
+ * \param   w wordlist of the simple statement
+ * \param   t type of the simple statement {BUILT-IN, EXEC}
+ * \return  returns 1 on success and 0 on error.
+ * \note    the wordlist won't be hardcopied to simple_stmt and the memory
+ *          allocated for it will be liberated by stmt_destroy()
+ */
+int stmt_set_simple(stmt_s *stmt, wordlist_s *w, enum stmt_type t);
+
+/**
+ * \brief   this functions set the stmt up to hold a if_stmt in stmt lol
+ * \param   t_case the true case of the new if_stmt
+ * \param   f_case the false case of the new if_stmt
+ * \return  1 on succeed. 0 on error
+ * \note    this function will call if_stmt_create
+ */
+int stmt_set_if_stmt(stmt_s *stmt, expr_s *cond, stmt_s *t_case, stmt_s *f_case);
+
+/**
+ * \brief   this functions set the stmt to hold a while_stmt in stmt lol
+ * \param   cond condition
+ * \param   t_case the stmt to execute if cond is true
+ * \return  1 on succeed. 0 on error
+ * \note    this function will call while_stmt_create
+ */
+int stmt_set_while_stmt(stmt_s *stmt, expr_s *cond, stmt_s *t_case);
+
+/**
+ * \brief   sets the next statement to be executed
+ * \param   stmt a statement
+ * \param   nxt the next statement
+ * \return  1 on succeed. 0 on error
+ * \note    
+ */
+int stmt_set_next(stmt_s *stmt, stmt_s *nxt);
 
 #endif
--- src/tools/kshell/kshell.c
+++ src/tools/kshell/kshell.c
@@ -5,6 +5,11 @@
 struct wordlist *wordlist_create(void)
 {
     wordlist_s *new = malloc(sizeof(*new));
+
+    if (!new) {
+        return NULL;
+    }
+
     new->nxt = NULL;
     
     return new;
@@ -35,6 +40,11 @@ void wordlist_print(struct wordlist *wordlist)
 struct wordlist *make_wordlist(const char * str)
 {
     wordlist_s *new = wordlist_create();
+
+    if (!new) {
+        return NULL;
+    }
+
     new->word = strdup(str);
 
     return new;
@@ -44,9 +54,174 @@ struct wordlist *make_wordlist(const char * str)
 struct wordlist *wordlist_pushfront(struct wordlist * l, char *str)
 {
     wordlist_s *new = make_wordlist(str);
+
+    if (!new) {
+        return NULL;
+    }
+
     new->nxt = l;
 
     return new;
+}
+
+
+stmt_s *stmt_create(void)
+{
+    stmt_s *new = malloc(sizeof(*new));
+
+    if (!new) {
+        return NULL;
+    }
+
+    new->nxt = NULL;
+    new->t   = NULL_TYPE;
+
+    return new;
+}
+
+void stmt_destroy(stmt_s *victim)
+{
+    if (!victim) return;
+
+    switch (victim->t) {
+        case EXEC_TYPE:
+        case BUILT_IN_TYPE :
+            wordlist_destroy(victim->stmt.simple_stmt);
+            break;
+        case WHILE_TYPE :
+            while_stmt_destroy(victim->stmt.while_stmt);
+            break;
+        case IF_TYPE :
+            if_stmt_destroy(victim->stmt.if_stmt);
+            break;
+        default: /* NULL_TYPE ? */
+            break;
+    }
+
+    free(victim);
+}
+
+
+stmt_s *if_stmt_create(void)
+{
+    struct if_stmt *new = malloc(sizeof(*new));
+
+    if (!new) {
+        return NULL;
+    }
+
+    new->condition  = NULL;
+    new->false_case = NULL;
+    new->true_case  = NULL;
+
+    return new;
+}
+
+
+void if_stmt_destroy(if_stmt_s *victim)
+{
+    if (!victim) return;
+
+    /**
+     * \todo destroy condition
+     */
+
+    if (victim->true_case) stmt_destroy(victim->true_case);
+
+    if (victim->false_case) stmt_destroy(victim->false_case);
+
+    free(victim);
+}
+
+
+stmt_s *while_stmt_create(void)
+{
+    struct while_stmt *new = malloc(sizeof(*new));
+
+    if (!new) {
+        return NULL;
+    }
+
+    new->condition = NULL;
+    new->execute   = NULL;
+
+    return new;
+}
+
+/**
+ * \brief   destroy an while_stmt
+ * \param   victim the while_stmt to destroy
+ * \note    this fuctions destroys the victim and all the 
+ *          conditional block it represents.
+ *          If stmt is NULL, this function has no effect.
+ *          this function may call stmt_destroy().
+ *          note that the nxt attribute will remain unmodified.
+ */
+void while_stmt_destroy(while_stmt_s *victim)
+{
+    if (!victim) return;
+    /**
+     * if (victim->condition) 
+     * \todo destroy condition
+     */
+
+    if (victim->execute) stmt_destroy(victim->execute);
+
+    free(victim);
+}
+
+
+int stmt_set_simple(stmt_s *stmt, wordlist_s *w, enum stmt_type t)
+{
+    if (!stmt || !w) return 0;
+
+    stmt->t = t;
+    stmt->stmt.simple_stmt = w;
+
+    return 1;
+}
+
+
+int stmt_set_if_stmt(stmt_s *stmt, expr_s *cond, stmt_s *t_case, stmt_s *f_case)
+{
+    if (!stmt || !t_case || !cond) return 0;
+
+    if_stmt_s *new = if_stmt_create();
+    new->condition = cond;
+    new->true_case = t_case;
+    new->false_case = f_case;
+    stmt->stmt.if_stmt = new;
+
+    return 1;
+}
+
+
+int stmt_set_while_stmt(stmt_s *stmt, expr_s *cond, stmt_s *t_case)
+{
+    if (!stmt || !cond || !t_case) return 0;
+
+    while_stmt_s *new = while_stmt_create();
+    new->condition = cond;
+    new->execute = t_case;
+    stmt->stmt.while_stmt = new;
+
+    return 1;
+}
+
+/**
+ * \brief   sets the next statement to be executed
+ * \param   stmt a statement
+ * \param   nxt the next statement
+ * \return  1 on succeed. 0 on error
+ * \note    
+ */
+int stmt_set_next(stmt_s *stmt, stmt_s *nxt)
+{
+    if (!stmt || !nxt) return 0;
+
+    stmt->nxt = nxt;
+
+    return 1;
 }
 
 
