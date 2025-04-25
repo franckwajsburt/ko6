bcf2fe71914065ed0502eb0da9fd032f557f9bda
Franck Wajsburt <franck.wajsburt@lip6.fr>
2025-04-25 23:52:15 +0200

    permissions & types for the files

--- src/soft/common/vfs_stat.h
+++ src/soft/common/vfs_stat.h
@@ -6,17 +6,58 @@
 
   \file     vfs_stat.h
   \author   Franck Wajsburt
-  \brief    Defines file type and permission flags for VFS and user applications.
+  \brief    Defines types, modes and permission flags for VFS and user applications.
 
-  This header provides symbolic constants used to describe file types and permissions,
-  compatible with POSIX macros (S_IFREG, S_IRUSR, etc.). 
-  It is intended for both kernel and userland use in ko6.
-  The values are POSIX, but it doesn't matter
+  This header provides: 
+  
+  * Basic types used for file system metadata, compatible with POSIX conventions.
+    These types are shared between the kernel and user space (libc), 
+    and used mainly for struct stat and related system calls.
+ 
+  * Structure containing file attributes used by VFS API function (similar to POSIX struct stat).
+  
+  * Symbolic constants used to describe file types and permissions,
+    compatible with POSIX macros (S_IFREG, S_IRUSR, etc.). 
+    It is intended for both kernel and userland use in ko6.
+    The values are POSIX, but it doesn't matter
 
 \*------------------------------------------------------------------------------------------------*/
 
 #ifndef _VFS_STAT_H_
 #define _VFS_STAT_H_
+
+//--------------------------------------------------------------------------------------------------
+// Basic types
+//--------------------------------------------------------------------------------------------------
+
+typedef unsigned int mode_t;   ///< File mode (type + permissions)
+typedef unsigned int ino_t;    ///< Inode number
+typedef unsigned int nlink_t;  ///< Number of hard links
+typedef unsigned int uid_t;    ///< User ID
+typedef unsigned int dev_t;    ///< Device ID (major and minor combined)
+typedef unsigned long off_t;   ///< File size or offset
+typedef unsigned long time_t;  ///< Time in seconds since UNIX epoch
+
+//--------------------------------------------------------------------------------------------------
+// Structure containing file attributes 
+//--------------------------------------------------------------------------------------------------
+
+struct stat {
+    dev_t     st_dev;     ///< Device ID containing the file (minor number of the blockdev)
+    ino_t     st_ino;     ///< Inode number
+    mode_t    st_mode;    ///< File type and permissions
+    nlink_t   st_nlink;   ///< Number of hard links
+    uid_t     st_uid;     ///< User ID of owner
+    dev_t     st_rdev;    ///< Device ID (S_IFCHR: chardev's minor nb; S_IFBLK: blockdev's minor nb)
+    off_t     st_size;    ///< Total size, in bytes
+    time_t    st_atime;   ///< Time of last access
+    time_t    st_mtime;   ///< Time of last modification
+    time_t    st_ctime;   ///< Time of last status change
+};
+
+//--------------------------------------------------------------------------------------------------
+// modes & permissions
+//--------------------------------------------------------------------------------------------------
 
 #define S_IFMT   0xF000                                     ///< Bitmask for the file type field
 
@@ -46,6 +87,7 @@
 
 #define S_IRWXOTH  (S_IROTH|S_IWOTH|S_IXOTH)                ///< others permission: RWX
 #define S_IRWXUSR  (S_IRUSR|S_IWUSR|S_IXUSR)                ///< owner permission: RWX
+#define S_IRWXALL  (S_IRWXOTH|S_IRWXUSR)                    ///< permission: RWX for all
                     
 #endif//_VFS_STAT_H_
 
